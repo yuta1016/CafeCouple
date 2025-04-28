b989d2ad9e90e829fe8b8492b9d3bb58a891ee16
yuta <yutasosukeaoto1016@gmail.com>
2025-04-28 21:05:07 +0900

    雛形完成

--- index_couple.php
+++ index_couple.php
@@ -2,8 +2,24 @@
 <html>
 <head>
 <meta charset='utf-8'>
+<!--
+大きさ指定。phpはhtmlと違ってpcのサイズにならなそう
+スマホサイズに設定
+-->
+<meta name="viewport"
+   content="width=320,
+      height=480,
+      initial-scale=1.0,
+      minimum-scale=1.0,
+      maximum-scale=2.0,
+      user-scalable=yes" />
+
+
 <title>HTML内でのPHP処理</title>
 <link href="style.css" rel="stylesheet">
+
+
+
 </head>
 <body>
 <h1><?php echo 'タイトルです'; ?></h1>
@@ -33,24 +49,32 @@
 <!--カフェ情報-->
 <?php
     $MatsueCafes=[
-        ["服部珈琲", "MAMP-PRO-Logo.png", "何とかだからおすすめ"],
-        ["pâtisserie J.KOWARI","imageBBB","aaaaa"],
-        ["Pa.U SOFT CREAM", "imageCCC","sadgasdg"],
+        [
+            "服部珈琲",
+            "hattorikohi.jpg",
+            "服部珈琲" ,
+            "広々とした落ち着いた空間で、ケーキやサラダなど様々な物が味わえるカフェ。木の温もりを感じる店内は、ソファ席も多く、ふたりでゆっくり過ごしたいデートにぴったりです", 
+            '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12996.586914359143!2d133.02689731121066!3d35.475913320038664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3557050eed5ea9c9%3A0x970d2c13ed1eefab!2z5pyN6YOo54-I55Cy5bel5oi_IOm7kueUsOW6lw!5e0!3m2!1sja!2sjp!4v1745841681399!5m2!1sja!2sjp" width="320" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        ],
+        ["スカラベ別邸","sukarabebebettei.jpg","スカラベ別邸","和モダンな空間でゆったり過ごせる、隠れ家のようなカフェ。上品で落ち着いた店内は、大人のデートにぴったり。目の前でモンブランをしてくれるやつあるよ!(語彙力)。"]
     ]
 ?>
 
 
 <header>
-    <h3 class="pos-left">カップルおすすめCafe</h3>
+    <h3 class="pos-left site-name">【松江市】カップルおすすめCafe</h3>
 
 </header>
 
 <main>
-    <h4 class="pos-center heading">カップルがデートに行く時におすすめしたいカフェを紹介！</h4>
-    <div class="detail pos-center">
-        <p>こんにちは</p>
-        <p>島根県でデートする時。。。。。。。。。。。</p>
+    <h4 class="title pos-center">デート時に行くおすすめしたい【松江市】カフェを紹介！</h4>
+    <div class="detail">
+        <p> 松江でデートするなら、カフェ一択！<br>
+            松江でデートするとなったら行くとこ無いよね、、だったらカフェに行こう！<br>
+            本記事では、カップルにぴったりな松江市内のおしゃれカフェをたっぷり紹介しています。ふたりでのんびり過ごしたいとき、ちょっと特別な気分を味わいたいときにぴったりのお店ばかり。次のデート先に迷ったら、ぜひ参考にして、素敵な時間を過ごしてくださいね！</p>
     </div>
+
+    <hr>
 
     <div class="cafe-content">
     <?php
@@ -59,11 +83,15 @@
             //名前
             echo "<h4 class='cafe-name'>$vale[0]</h4>";
             //写真
-            echo "<img class='cafe-img' src='./$vale[1]' alt='画像'>";
+            echo "<div class='pos-center'><img class='cafe-img' src='./images/$vale[1]' alt='$vale[2]'></div>";
             //説明
-            echo "<div class='cafe-description'><p>$vale[2]<p></div>";
+            echo "<div class='cafe-description'><p>$vale[3]<p></div>";
+
+            echo $vale[4];
 
             echo "</div>";
+
+            echo "<hr>";
         }
     ?>
     </div>
