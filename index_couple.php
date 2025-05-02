68e1c0d0cd33a26f498fccafcd9f4393e2357074
yuta <yutasosukeaoto1016@gmail.com>
2025-05-02 10:58:56 +0900

    Googleコンソール追加

--- index_couple.php
+++ index_couple.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0KRS846BKY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0KRS846BKY');
+</script>
+
 <meta charset='utf-8'>
 <!--
 大きさ指定。phpはhtmlと違ってpcのサイズにならなそう
